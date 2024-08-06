a67044b1e01b0d730df4f67d431f600c5f21e3ce
LakshmiNarasimhaSarma <61181712+LakshmiNarasimhaSarma@users.noreply.github.com>
2024-08-06 21:59:25 +0530

    Update TestGreeter.java

--- Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,13 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "AWS Devops";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
